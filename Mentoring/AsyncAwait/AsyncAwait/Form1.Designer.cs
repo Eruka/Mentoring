dfad238c7f985d77c5d2f19401939826c787b3e8
Yuliya Melnikava <Yuliya_Melnikava@epam.com>
2015-11-30 01:05:55 +0300

    Added AsyncAwait task

---
+++ Mentoring/AsyncAwait/AsyncAwait/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace AsyncAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addNewCtrlBtn = new System.Windows.Forms.Button();
+            this.mainPnl = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // addNewCtrlBtn
+            // 
+            this.addNewCtrlBtn.Location = new System.Drawing.Point(12, 639);
+            this.addNewCtrlBtn.Name = "addNewCtrlBtn";
+            this.addNewCtrlBtn.Size = new System.Drawing.Size(75, 23);
+            this.addNewCtrlBtn.TabIndex = 5;
+            this.addNewCtrlBtn.Text = "Add";
+            this.addNewCtrlBtn.UseVisualStyleBackColor = true;
+            this.addNewCtrlBtn.Click += new System.EventHandler(this.addNewCtrlBtn_Click);
+            // 
+            // mainPnl
+            // 
+            this.mainPnl.AutoScroll = true;
+            this.mainPnl.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.mainPnl.Location = new System.Drawing.Point(12, 12);
+            this.mainPnl.Name = "mainPnl";
+            this.mainPnl.Size = new System.Drawing.Size(1097, 621);
+            this.mainPnl.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 674);
+            this.Controls.Add(this.mainPnl);
+            this.Controls.Add(this.addNewCtrlBtn);
+            this.Name = "Form1";
+            this.Text = "AsyncAwaitTask";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addNewCtrlBtn;
+        private System.Windows.Forms.Panel mainPnl;
+    }
+}
+
---
+++ Mentoring/AsyncAwait/AsyncAwait/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+    public partial class Form1 : Form
+    {
+        readonly Dictionary<Control, CancellationTokenSource> controlsWithCts = new Dictionary<Control, CancellationTokenSource>() ;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelLoadingBtn_Click(object sender, EventArgs e)
+        {
+            var parent = ((Button)sender).Parent;
+            var cts = controlsWithCts[parent];
+
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
+        }
+
+        private async void loadPageBtn_Click(object sender, EventArgs e)
+        {
+
+            var parent = ((Button)sender).Parent;
+            var urlTBox = (TextBox)parent.Controls[0];
+            var pageContentTBox = (RichTextBox)parent.Controls[3];
+
+            pageContentTBox.Clear();
+            pageContentTBox.Text = "Loading";
+            var cts = new CancellationTokenSource();
+            var cancellationtoken = cts.Token;
+
+            if (!controlsWithCts.ContainsKey(parent))
+            {
+                controlsWithCts.Add(parent, cts);
+            } else
+            {
+                controlsWithCts[parent] = cts;
+            }
+
+            Task.Factory.StartNew(async () =>
+             {
+                 while (true)
+                 {
+                     cancellationtoken.ThrowIfCancellationRequested();
+                     await Task.Delay(100);
+                     pageContentTBox.Invoke(new MethodInvoker(() => pageContentTBox.Text += "."));
+                 }
+             }, cancellationtoken);
+
+            try
+            {
+                string data = await GetDataAsync(urlTBox.Text.Trim(), cancellationtoken);
+                cts.Cancel();
+                pageContentTBox.Text += "\n" + data;
+            }
+            catch (OperationCanceledException)
+            {
+                pageContentTBox.Text += "download canceled";
+            }
+            catch (Exception ex)
+            {
+                cts.Cancel();
+                pageContentTBox.Text += ex.Message;
+            }
+        }
+
+        private async Task<string> GetDataAsync(string url, CancellationToken ct)
+        {
+            HttpClient client = new HttpClient();
+            await Task.Delay(5000);
+            HttpResponseMessage responce = await client.GetAsync(url, ct);
+            return await responce.Content.ReadAsStringAsync();
+        }
+
+        private void addNewCtrlBtn_Click(object sender, EventArgs e)
+        {
+            int count = mainPnl.Controls.Count;
+            var panel = new Panel() { Height = 140, Width = 1040, BackColor  = Color.Gray , Location = new Point(10, count * (140 + 10))};
+            var textBox = new TextBox() { Text = "http://tut.by", Location = new Point(15,10)};
+            var loadButton = new Button() { Text = "Load", Location = new Point(15, 45), BackColor = Color.LightGray };
+            var cancelButton = new Button() { Text = "Cancel", Location = new Point(105, 45), BackColor = Color.LightGray };
+            var contentTextBox = new RichTextBox() { Location = new Point(290, 10), Width = 730, Height = 115 };
+            loadButton.Click += loadPageBtn_Click;
+            cancelButton.Click += cancelLoadingBtn_Click;
+            panel.Controls.AddRange(new Control[] { textBox, loadButton, cancelButton, contentTextBox });
+            mainPnl.Controls.Add(panel);
+        }
+    }
+}
